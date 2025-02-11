95774f6c21e779f7da97494beda90619013db8c2
github.com:NafisahsJavaLab/github-workflow.git <njhaneef@gmail.com>
2025-02-11 13:57:00 -0500

    WorkShop block7

---
+++ Code.js
@@ -0,0 +1,27 @@
+/* this code will use equations to produce the three numbers that make up a combination
+create a variable and assign it the value of a subtraction equation
+create a second variable and assign it the value of a division equation
+create a third variable and assign it the value of an addition equation
+create a variable "vaultCode" that list the first three variables as a string
+create a message that contains a string
+use console.log() to display the message and vaultCode*/
+
+
+
+const number1 = (15 - 5) 
+
+
+const number2 = (80 / 2)
+
+
+const number3 = (17 + 22)
+
+
+const vaultCode = (`${number1} - ${number2} - ${number3}`)
+
+
+ const message ="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+  
+ console.log(message);
+console.log(vaultCode);
+
